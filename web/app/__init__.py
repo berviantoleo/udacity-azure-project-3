460c65b00665c3f579c1127abb610b3be00ab419
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-03-28 03:28:47 +0700

    Upgrade

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy 
-from azure.servicebus import QueueClient
+from flask_sqlalchemy import SQLAlchemy
+from azure.servicebus import ServiceBusClient
 
 
 app = Flask(__name__)
@@ -9,9 +9,8 @@ app.config.from_object('config.DevelopmentConfig')
 
 app.secret_key = app.config.get('SECRET_KEY')
 
-queue_client = QueueClient.from_connection_string(app.config.get('SERVICE_BUS_CONNECTION_STRING'),
-                                                 app.config.get('SERVICE_BUS_QUEUE_NAME'))
-
+client = ServiceBusClient.from_connection_string(app.config.get('SERVICE_BUS_CONNECTION_STRING'))
+queue_client = client.get_queue_sender(app.config.get('SERVICE_BUS_QUEUE_NAME'))
 db = SQLAlchemy(app)
 
 from . import routes
--- web/app/routes.py
+++ web/app/routes.py
@@ -2,7 +2,7 @@ from app import app, db, queue_client
 from datetime import datetime
 from app.models import Attendee, Conference, Notification
 from flask import render_template, session, request, redirect, url_for, flash, make_response, session
-from azure.servicebus import Message
+from azure.servicebus import ServiceBusMessage
 import logging
 import sys
 
@@ -66,7 +66,7 @@ def notification():
             db.session.add(notification)
             db.session.commit()
 
-            message = Message(notification.id)
+            message = ServiceBusMessage(notification.id)
             queue_client.send_message(message)
 
             return redirect('/Notifications')
