c876634ea37a3ad66e9a63767ea99715ced3e118
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-03-28 03:56:20 +0700

    change to env

--- web/config.py
+++ web/config.py
@@ -13,7 +13,7 @@ class BaseConfig:
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
     SERVICE_BUS_CONNECTION_STRING = os.getenv('SERVICE_BUS_CONNECTION_STRING')
-    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    SERVICE_BUS_QUEUE_NAME = os.getenv('SERVICE_BUS_QUEUE_NAME')
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
 
